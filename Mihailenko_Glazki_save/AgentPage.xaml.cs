5756d2d59e07ad968707267595fa9e1f62eeb643
Artem Meha <ps4slim.astral6162@gmail.com>
2026-02-18 20:36:05 +0500

    Добавьте файлы проекта.

---
+++ Mihailenko_Glazki_save/AgentPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mihailenko_Glazki_save
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+            var currentAgent = MihailenkoGlazkiEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentAgent;
+            ComboType.SelectedIndex = 0;
+            ComboType2.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+
+        private void ComboType2_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ChangePriorityBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int maxPriority = 0;
+            foreach (Agent selectedAgent in AgentListView.SelectedItems)
+            {
+                if (selectedAgent.Priority > maxPriority)
+                {
+                    maxPriority = selectedAgent.Priority;
+                }
+            }
+            PriorChange prior = new PriorChange(maxPriority);
+            prior.ShowDialog();
+
+            int newPriority = Convert.ToInt32(prior.TBPriority.Text);
+            foreach (Agent agent in AgentListView.SelectedItems)
+            {
+                agent.Priority = newPriority;
+            }
+        }
+
+        private void addAgentBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AgentListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AgentListView.SelectedItems.Count > 0)
+            {
+                ChangePriorityBtn.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ChangePriorityBtn.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void UpdateAgents()
+        {
+            var currentAgents = MihailenkoGlazkiEntities.GetContext().Agent.ToList();
+            if (ComboType.SelectedIndex == 0) currentAgents = currentAgents.Where(p => (p.AgentTypeID >= 0 && p.AgentTypeID <= 6)).ToList();
+            if (ComboType.SelectedIndex == 1) currentAgents = currentAgents.Where(p => (p.AgentTypeID == 1)).ToList();
+            if (ComboType.SelectedIndex == 2) currentAgents = currentAgents.Where(p => (p.AgentTypeID == 2)).ToList();
+            if (ComboType.SelectedIndex == 3) currentAgents = currentAgents.Where(p => (p.AgentTypeID == 3)).ToList();
+            if (ComboType.SelectedIndex == 4) currentAgents = currentAgents.Where(p => (p.AgentTypeID == 4)).ToList();
+            if (ComboType.SelectedIndex == 5) currentAgents = currentAgents.Where(p => (p.AgentTypeID == 5)).ToList();
+            if (ComboType.SelectedIndex == 6) currentAgents = currentAgents.Where(p => (p.AgentTypeID == 6)).ToList();
+            if (ComboType2.SelectedIndex == 0) { }
+            if (ComboType2.SelectedIndex == 1) currentAgents = currentAgents.OrderBy(p => p.Title).ToList();
+            if (ComboType2.SelectedIndex == 2) currentAgents = currentAgents.OrderByDescending(p => p.Title).ToList();
+            if (ComboType2.SelectedIndex == 3) currentAgents = currentAgents.OrderBy(p => p.Discount).ToList();
+            if (ComboType2.SelectedIndex == 4) currentAgents = currentAgents.OrderByDescending(p => p.Discount).ToList();
+            if (ComboType2.SelectedIndex == 5) currentAgents = currentAgents.OrderBy(p => p.Priority).ToList();
+            if (ComboType2.SelectedIndex == 6) currentAgents = currentAgents.OrderByDescending(p => p.Priority).ToList();
+
+            currentAgents = currentAgents.Where(p =>
+            p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())
+            || p.Phone.Replace("+7", "8").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "")
+            .Contains(TBoxSearch.Text.Replace("+7", "8").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", ""))
+            || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            AgentListView.ItemsSource = currentAgents;
+        }
+    }
+}
